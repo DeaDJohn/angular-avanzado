7bc68de9c01e937f70f079b3ea48c4e16840dc45
Juanjo Fernandez <juanjofb86@gmail.com>
2018-07-30 19:20:28 +0200

    Se modifica el custom.js porque cuando cargas la pagina login y despues alguna pagina del panel no cargaba este js y para eso se añade el declaration function nombrefuncion() y se agrega al onInit para que pueda inicializar la funcion de ese js y poder hacer uso de esas funciones.

--- adminpro/src/app/pages/pages.component.ts
+++ adminpro/src/app/pages/pages.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-
+//Declaramos la funcion que hemos hecho en el custom.js envolviendo a todo el contenido
+// para poder llamar a estas inicializaciones.
+declare function init_plugins();
 @Component({
   selector: 'app-pages',
   templateUrl: './pages.component.html',
@@ -10,6 +12,7 @@ export class PagesComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    init_plugins();
   }
 
 }
