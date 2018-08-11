019faa6a6161c48d736f1e0f0b2f35a064ea4672
Juanjo Fernandez <juanjofb86@gmail.com>
2018-08-11 11:53:18 +0200

    Instalacion de body-parser. Creada la logica para que liste los usarios dentro de la base de datos y para crear unos usarios (Pero en los usuarios no funciona las restricciones que he puesto en usuarios.js)

--- backend/app.js
+++ backend/app.js
@@ -1,10 +1,25 @@
 // Requiere
 var express = require('express');
 var mongoose = require('mongoose');
+var bodyParser = require('body-parser')
 
 
 // inicializar variables
 var app = express();
+
+
+
+// Body parser
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+// parse application/json
+app.use(bodyParser.json());
+
+
+// Importar rutas
+
+var appRoutes = require('./routes/app');
+var usuarioRoutes = require('./routes/usuario');
 
 
 // Conexión a la base de datos
@@ -17,16 +32,9 @@ mongoose.connection.openUri('mongodb://localhost:27017/hospitalDB', (err, res) =
 });
 
 
-
 // Rutas
-app.get('/', (req, res, next) => {
-
-    res.status(200).json({
-        ok: true,
-        mensaje: 'Petición realizada correctamente'
-    });
-
-});
+app.use('/usuario', usuarioRoutes);
+app.use('/', appRoutes);
 
 
 // Escuchar peticiones
