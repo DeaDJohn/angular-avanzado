06f7e1e41c7694540ab63ca4521150c297d93495
Juanjo Fernandez <juanjofb86@gmail.com>
2018-08-12 12:03:52 +0200

    Creada la lógica para el login.

--- backend/app.js
+++ backend/app.js
@@ -20,6 +20,7 @@ app.use(bodyParser.json());
 
 var appRoutes = require('./routes/app');
 var usuarioRoutes = require('./routes/usuario');
+var loginRoutes = require('./routes/login');
 
 
 // Conexión a la base de datos
@@ -34,6 +35,7 @@ mongoose.connection.openUri('mongodb://localhost:27017/hospitalDB', (err, res) =
 
 // Rutas
 app.use('/usuario', usuarioRoutes);
+app.use('/login', loginRoutes);
 app.use('/', appRoutes);
 
 
