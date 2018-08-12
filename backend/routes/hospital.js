91eba269204a02d22986428913f42e9ef2e96743
Juanjo Fernandez <juanjofb86@gmail.com>
2018-08-12 16:57:03 +0200

    Añadir paginacion en los hospitales, medicos y usuarios.

--- backend/routes/hospital.js
+++ backend/routes/hospital.js
@@ -14,7 +14,12 @@ var Hospital = require('../models/hospital');
 //============================
 app.get('/', (req, res, next) => {
 
+    var desde = req.query.desde || 0;
+    desde = Number(desde);
+
     Hospital.find({})
+        .skip(desde)
+        .limit(5)
         .populate('usuario', 'nombre email')
         .exec(
             (err, hospitales) => {
@@ -25,9 +30,12 @@ app.get('/', (req, res, next) => {
                         errors: err
                     });
                 }
-                res.status(200).json({
-                    ok: true,
-                    hospitales: hospitales
+                Hospital.count({}, (err, conteo) => {
+                    res.status(200).json({
+                        ok: true,
+                        hospitales: hospitales,
+                        total: conteo
+                    });
                 });
 
             }
--- backend/routes/medico.js
+++ backend/routes/medico.js
@@ -14,7 +14,12 @@ var Medico = require('../models/medico');
 //
 app.get('/', (req, res, next) => {
 
+    var desde = req.query.desde || 0;
+    desde = Number(desde);
+
     Medico.find({})
+        .skip(desde)
+        .limit(5)
         .populate('usuario', 'nombre email')
         .populate('hospital')
         .exec(
@@ -26,10 +31,14 @@ app.get('/', (req, res, next) => {
                         errors: err
                     });
                 }
-                res.status(200).json({
-                    ok: true,
-                    medicos: medicos
+                Medico.count({}, (err, conteo) => {
+                    res.status(200).json({
+                        ok: true,
+                        medicos: medicos,
+                        total: conteo
+                    });
                 });
+
 
             }
         );
