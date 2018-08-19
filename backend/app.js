16fe6cd5e72ac7085262c0a927709e8c597774c3
Juanjo Fernandez <juanjofb86@gmail.com>
2018-08-19 10:53:50 +0200

    Añadida la busqueda en usuarios, medicos y hospitales.

--- backend/app.js
+++ backend/app.js
@@ -23,6 +23,7 @@ var usuarioRoutes = require('./routes/usuario');
 var loginRoutes = require('./routes/login');
 var hospitalRoutes = require('./routes/hospital');
 var medicoRoutes = require('./routes/medico');
+var busquedaRoutes = require('./routes/busqueda');
 
 
 // Conexión a la base de datos
@@ -40,6 +41,7 @@ app.use('/usuario', usuarioRoutes);
 app.use('/login', loginRoutes);
 app.use('/hospital', hospitalRoutes);
 app.use('/medico', medicoRoutes);
+app.use('/busqueda', busquedaRoutes);
 
 app.use('/', appRoutes);
 
---
+++ backend/routes/busqueda.js
@@ -0,0 +1,95 @@
+var express = require('express');
+
+var app = express();
+
+var Hospital = require('../models/hospital');
+var Medico = require('../models/medico');
+var Usuario = require('../models/usuario');
+
+// Rutas
+app.get('/todo/:busqueda', (req, res, next) => {
+
+    var busqueda = req.params.busqueda;
+    // si no se hace esto de la expresion regular
+    // busca la expresion completa que se pase
+    // y con la 'i' hace que no sea case Sensitive
+    var regex = new RegExp(busqueda, 'i');
+
+
+
+    Promise.all([
+            buscarHospitales(busqueda, regex),
+            buscarMedicos(busqueda, regex),
+            buscarUsuarios(busqueda, regex),
+        ])
+        .then(respuestas => {
+            res.status(200).json({
+                ok: true,
+                hospitales: respuestas[0],
+                medicos: respuestas[1],
+                usuarios: respuestas[2]
+            });
+        })
+
+
+});
+
+function buscarHospitales(busqueda, regex) {
+
+    return new Promise((resolve, reject) => {
+
+        Hospital.find({ nombre: regex })
+            // obtener el usuario y el email de quien lo creo
+            .populate('usuario', 'nombre email')
+            .exec((err, hospitales) => {
+
+                if (err) {
+                    reject('Error al cargar hospitales', err);
+                } else {
+                    resolve(hospitales);
+                }
+
+            });
+
+    });
+}
+
+function buscarMedicos(busqueda, regex) {
+
+    return new Promise((resolve, reject) => {
+
+        Medico.find({ nombre: regex })
+            // obtener el usuario y el email de quien lo creo
+            .populate('usuario', 'nombre email')
+            .exec((err, medicos) => {
+
+                if (err) {
+                    reject('Error al cargar medicos', err);
+                } else {
+                    resolve(medicos);
+                }
+
+            });
+
+    });
+}
+
+function buscarUsuarios(busqueda, regex) {
+
+    return new Promise((resolve, reject) => {
+
+        Usuario.find({}, 'nombre email role')
+            .or([{ 'nombre': regex }, { 'email': regex }])
+            .exec((err, usuarios) => {
+                if (err) {
+                    reject('Error al cargar usuario', err);
+                } else {
+                    resolve(usuarios);
+                }
+            });
+
+    });
+}
+
+
+module.exports = app;
