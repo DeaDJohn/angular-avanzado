2443c5e70286a77c3682e0e08582d4fda7521f71
Juanjo Fernandez <juanjofb86@gmail.com>
2018-08-24 11:55:55 +0200

    Guardar el login en localStorage

--- adminpro/src/app/login/login.component.ts
+++ adminpro/src/app/login/login.component.ts
@@ -30,8 +30,8 @@ export class LoginComponent implements OnInit {
     let usuario = new Usuario( null, forma.value.email, forma.value.password)
 
     this._usuarioService.login( usuario, forma.value.recuerdame )
-    .subscribe( resp => {
-      console.log( resp );
+    .subscribe( correcto => {
+      this.router.navigate(['/dashboard']);
     });
 
     // this.router.navigate(['/dashboard']);
